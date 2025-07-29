85c7865db32e15a00c1161d84840f4d3bd6cdaad
Ahmad Raza <imarslan7061@gmail.com>
2025-07-29 14:19:37 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-zinc-950 text-white">
+      <h1 className="text-4xl text-center font-medium">
+        React Query | TanStack Query
+      </h1>
+    </div>
+  );
+}
+
+export default App;
